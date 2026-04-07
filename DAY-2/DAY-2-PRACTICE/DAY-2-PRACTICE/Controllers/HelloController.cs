8e7a467d613c43ed7c3d0ffd7785f8166dc211a6
Kshitij1ghk <kshitijwk@gmail.com>
2026-04-07 14:42:46 +0530

    -added day-2 git exercise

---
+++ DAY-2/DAY-2-PRACTICE/DAY-2-PRACTICE/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAY_2_PRACTICE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public IActionResult GetHello()
+        {
+            return Ok("HelloWorld"); ;
+        }
+    }
+}
---
+++ DAY-2/DAY-2-PRACTICE/DAY-2-PRACTICE/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAY_2_PRACTICE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateUser()
+        {
+            var user = new
+            {
+                Name = "Kshitij",
+                Email = "kshitij@gmail.com",
+                Age = 20
+            };
+
+            return Created("/api/user", user);
+        }
+    }
+}
