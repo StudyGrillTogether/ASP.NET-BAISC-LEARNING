c939d04725ef828f6fd3f61d751c3ae6019dc39c
Kshitij1ghk <kshitijwk@gmail.com>
2026-04-12 10:57:17 +0530

    Day 10 - Web API with GET and POST endpoints

---
+++ DAY-10/DAY-10-PRACTICE/DAY-10-PRACTICE/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using DAY_10_PRACTICE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAY_10_PRACTICE.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> _users = new List<User>
+        {
+            new User { Id = 1, Name = "Kshitij", Email = "k@gmail.com" },
+            new User { Id = 2, Name = "Solace", Email = "sol@gmail.com" }
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            _users.Add(user);
+            return Created("", user);
+        }
+    }
+}
---
+++ DAY-10/DAY-10-PRACTICE/DAY-10-PRACTICE/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DAY_10_PRACTICE.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
