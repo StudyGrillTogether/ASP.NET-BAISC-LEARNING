cef0fb0bec31502e1dfe041efcabee3ed1137deb
Kshitij1ghk <kshitijwk@gmail.com>
2026-04-13 16:48:24 +0530

    -added data anotations to the user model

---
+++ DAY-12/DAY-12-PRACTICE/DAY-12-PRACTICE/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAY_12_PRACTICE.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name is Required")]
+        [StringLength(50,ErrorMessage ="Name cannot exceed 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress(ErrorMessage ="Invalid email format")]
+        public string Email { get; set; }
+
+    }
+}
