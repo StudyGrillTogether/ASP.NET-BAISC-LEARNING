d70f4f0098ae936d3eb167c479188191277e7d26
Kshitij1ghk <kshitijwk@gmail.com>
2026-04-13 14:52:51 +0530

    Day 11 - Route params, Query params, PUT and DELETE endpoints

---
+++ DAY-11/DAY-11-PRACTICE/DAY-11-PRACTICE/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using DAY_11_PRACTICE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAY_11_PRACTICE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public static List<User> _users = new List<User>()
+        {
+            new User { Id = 1, Name = "Kshitij", Email = "k@gmail.com" },
+            new User { Id = 2, Name = "Solace", Email = "sol@gmail.com" }
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_users);
+        }
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id)
+        {
+            var user = _users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            _users.Add(user);
+            return Created("", user);
+        }
+
+        //search-query parameter
+        [HttpGet("search")]
+        public IActionResult Search(string name)
+        {
+            var result = _users.Where(u => u.Name.Contains(name)).ToList();
+            return Ok(result);
+        }
+        //Update
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, User updatedUser)
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Name = updatedUser.Name;
+            user.Email = updatedUser.Email;
+            return Ok(user);
+        }
+
+        //DELETE
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(int id)
+        {
+            var user=_users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+
+            _users.Remove(user);
+            return Ok("User deleted successfully");
+        }
+    }
+}
---
+++ DAY-11/DAY-11-PRACTICE/DAY-11-PRACTICE/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DAY_11_PRACTICE.Models
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } 
+
+        public string Email {  get; set; }
+    }
+}
