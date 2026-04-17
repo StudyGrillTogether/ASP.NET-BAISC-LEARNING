f713f23ab052089afb5fcc3b862c5c78182c1fea
Kshitij1ghk <kshitijwk@gmail.com>
2026-04-17 11:40:51 +0530

    feat: initialize ASP.NET Core web application with HelloWorld controller and OpenAPI documentation

--- DAY-1/DAY-1-PRACTICE/DAY-1-PRACTICE/Controllers/HelloWorldController.cs
+++ DAY-1/DAY-1-PRACTICE/DAY-1-PRACTICE/Controllers/HelloWorldController.cs
@@ -3,14 +3,29 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DAY_1_PRACTICE.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("api/[controller]")] //this is the route template
+    //it tells the application that this controller will handle requests that start with "api/HelloWorld"
+    [ApiController] //this is an attribute that tells the application that this class is a controller
     public class HelloWorldController : ControllerBase
     {
-        [HttpGet]
-        public string Get()
-        {
-            return "Hello World";
+        [HttpGet] //this is an attribute that tells the application that this method will handle GET requests
+        public IActionResult Get()
+        //this is a method that will handle GET requests
+        //it returns an IActionResult which is an interface that represents the result of an action method
+            return Ok("hello world");//respinse is sent to to the browser or swagger and output is displayed
         }
     }
 }
+//User hits /api/HelloWorld
+//↓
+//Request goes to app
+//↓
+//app.MapControllers() routes it
+//↓
+//HelloWorldController is found
+//↓
+//Get() method is called
+//↓
+//"Hello World" returned
+//↓
+//Browser shows response
--- DAY-1/DAY-1-PRACTICE/DAY-1-PRACTICE/Program.cs
+++ DAY-1/DAY-1-PRACTICE/DAY-1-PRACTICE/Program.cs
@@ -1,14 +1,21 @@
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+//Creates the application builder
+//Prepares configuration, services, environment
+//starting point of the app
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers();//registers controllers into the app 
+//without this controllers wont work at all
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+//adds openApi/Swagerr services 
+//this is used to generate the documentation of the api
 var app = builder.Build();
+//builds the application from configuration or final app
+//this is the actual running application
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,11 +23,17 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
+//enables open api endpoint 
+//this is only enabled in development mode
 
 app.UseHttpsRedirection();
+//redirects http to https
 
 app.UseAuthorization();
+//enables authorization
 
 app.MapControllers();
+//connects incoming requests to the correct controller
 
 app.Run();
+//starts the application
